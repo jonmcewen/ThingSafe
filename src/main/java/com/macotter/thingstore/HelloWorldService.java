5a6b8c41aaf8efbba25b3d63eba58c642bc25367
Jon McEwen <jon@Jonathans-MacBook-Pro.local>
2013-03-24 14:21:43 +0000

    hello-world

---
+++ src/main/java/com/macotter/thingstore/HelloWorldService.java
@@ -0,0 +1,30 @@
+package com.macotter.thingstore;
+
+import com.macotter.thingstore.HelloWorldConfiguration;
+import com.macotter.thingstore.core.TemplateHealthCheck;
+import com.macotter.thingstore.resources.HelloWorldResource;
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+
+public class HelloWorldService extends Service<HelloWorldConfiguration> {
+	public static void main(String[] args) throws Exception {
+		new HelloWorldService().run(args);
+	}
+
+	@Override
+	public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+		bootstrap.setName("hello-world");
+	}
+
+	@Override
+	public void run(HelloWorldConfiguration configuration,
+			Environment environment) {
+		final String template = configuration.getTemplate();
+		final String defaultName = configuration.getDefaultName();
+		environment.addResource(new HelloWorldResource(template, defaultName));
+		environment.addHealthCheck(new TemplateHealthCheck(template));
+
+	}
+
+}
